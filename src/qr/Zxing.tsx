0e2d255623821dfaf5fe18bf11e3fce243e667c3
tukky-14 <bigoonage0104@gmail.com>
2023-05-29 23:07:54 +0900

    zxingの処理を追加

--- src/qr/Zxing.tsx
+++ src/qr/Zxing.tsx
@@ -1,43 +1,47 @@
-import { useRef, useEffect, useState, useCallback } from 'react';
-import Webcam from 'react-webcam';
+import { BrowserMultiFormatReader } from '@zxing/library';
+import { useRef, useEffect, useState } from 'react';
 
 const Zxing = () => {
-    const videoConstraints = {
-        width: 720,
-        height: 720,
-        facingMode: 'environment',
-    };
-
-    const [imageSrc, setImageSrc] = useState('');
-    const webcamRef = useRef<any>(null);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [resultText, setResultText] = useState('');
+    const codeReader = new BrowserMultiFormatReader();
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            const imageSrc = webcamRef.current.getScreenshot();
-            console.log('imageSrc:', imageSrc);
-            setImageSrc(imageSrc);
-        }, 1000);
-        return () => clearInterval(interval);
+        navigator.mediaDevices
+            .enumerateDevices()
+            .then((devices) => {
+                const videoInputDevices = devices.filter((device) => device.kind === 'videoinput');
+                const sourceId = videoInputDevices[0].deviceId;
+                codeReader.decodeFromVideoDevice(sourceId, videoRef.current, (result, err) => {
+                    if (result) {
+                        console.log(result);
+                        setResultText(result.getText());
+                        // setResultText(result.text);
+                    }
+                    if (err) {
+                        // console.error(err);
+                        // alert('error');
+                    }
+                });
+            })
+            .catch((err) => console.error(err));
+
+        // cleanup function
+        return () => {
+            codeReader.reset();
+        };
     }, []);
 
     return (
         <div className="mt-5">
             <div className="m-auto text-center text-xl">zxing</div>
             <div className="px-2">
-                <Webcam
-                    className="m-auto"
-                    audio={false}
-                    width={720}
-                    height={720}
-                    ref={webcamRef}
-                    screenshotFormat="image/jpeg"
-                    videoConstraints={videoConstraints}
-                />
+                <video className="m-auto" ref={videoRef} style={{ width: '300px' }} />
             </div>
             <div className="px-2 text-center">
                 <div>【読取結果】</div>
                 <div className="m-auto px-2 w-full sm:w-1/2 border rounded h-20 bg-gray-100 overflow-scroll">
-                    {imageSrc}
+                    {resultText}
                 </div>
             </div>
         </div>
